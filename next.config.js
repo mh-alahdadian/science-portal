28303ace8acdbcc010cdac9196c2736109810bbf
MohammadHossein Alahdadian <mhalah15@gmail.com>
2024-02-14 12:22:49 +0330

    upgrade next and use turbo

--- next.config.js
+++ next.config.js
@@ -1,31 +1,37 @@
 const isDev = process.env.NODE_ENV !== 'production';
 const serverAddress = 'http://192.168.59.20';
 
+const svgLoaders = [
+  {
+    loader: '@svgr/webpack',
+    /** @type {import('@svgr/core').Config} */
+    options: {
+      svgoConfig: {
+        multipass: true,
+        plugins: [{ name: 'preset-default', params: { overrides: { removeViewBox: false } } }, 'prefixIds'],
+      },
+    },
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          as: '*.js',
+          loaders: svgLoaders,
+        },
+      },
+    },
+  },
   /** @param {import('webpack').Configuration} config */
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: [/\.[jt]sx?$/, /^$/],
-      use: [
-        {
-          loader: '@svgr/webpack',
-          /** @type {import('@svgr/core').Config} */
-          options: {
-            svgoConfig: {
-              multipass: true,
-              plugins: [
-                {
-                  name: 'preset-default',
-                  params: { overrides: { removeViewBox: false } },
-                },
-                'prefixIds',
-              ],
-            },
-          },
-        },
-      ],
+      use: svgLoaders,
     });
 
     return config;
