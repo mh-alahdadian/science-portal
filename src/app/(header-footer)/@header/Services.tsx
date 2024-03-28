6eb9594900a1cd974c24b769f4eccfb038cc5082
MohammadHossein Alahdadian <mhalah15@gmail.com>
2024-03-28 11:38:46 +0330

    update swagger files and dependencies

--- src/app/(header-footer)/@header/Services.tsx
+++ src/app/(header-footer)/@header/Services.tsx
@@ -1,34 +1,32 @@
 import { queryClient, queryService } from '@/api';
-import { useProfile } from '@/hooks';
 import { ServiceMenu } from './ServiceMenu';
 
 interface Page {
-    title: string;
-    path: string;
-  }
-  
-  
+  title: string;
+  path: string;
+}
+
 const services: Page[] = [
-    { title: 'اخبار', path: 'news' },
-    { title: 'انجمن', path: 'forum' },
-    { title: 'کتابخانه', path: 'library' },
-    { title: 'وبلاگ', path: 'blog' },
-    { title: 'گالری', path: 'gallery' },
-  ];
-  
-  function getParams(scopeId: string) {
-    return { params: Number.isInteger(+scopeId) ? { query: { scopeId } } : {} } as any;
-  }
-  
-  const emptyArr: any[] = [];
-  
-export default async function Services({scopeId}: {scopeId: string}) {
-    const servicesItems = await Promise.all([
-        queryClient.fetchQuery(queryService('news:/categories/scopes', getParams(scopeId))),
-        queryClient.fetchQuery(queryService('forum:/categories/scopes', getParams(scopeId))),
-      ]).catch(() => []);
-    
-      return (
+  { title: 'اخبار', path: 'news' },
+  { title: 'انجمن', path: 'forum' },
+  { title: 'کتابخانه', path: 'library' },
+  { title: 'وبلاگ', path: 'blog' },
+  { title: 'گالری', path: 'gallery' },
+];
+
+function getParams(scopeId: string) {
+  return { params: Number.isInteger(+scopeId) ? { query: { scopeId } } : {} } as any;
+}
+
+const emptyArr: any[] = [];
+
+export default async function Services({ scopeId }: { scopeId: string }) {
+  const servicesItems = await Promise.all([
+    queryClient.fetchQuery(queryService('news:/v1/scope/{scopeId}/categories', getParams(scopeId))),
+    queryClient.fetchQuery(queryService('forum:/v1/scope/{scopeId}/categories', getParams(scopeId))),
+  ]).catch(() => []);
+
+  return (
     <>
       {services.map((page, index) => (
         <ServiceMenu key={page.title} path={page.path} title={page.title} items={servicesItems[index] || emptyArr} />
