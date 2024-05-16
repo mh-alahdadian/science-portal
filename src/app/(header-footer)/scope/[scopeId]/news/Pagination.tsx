c53a69f1a8249f2d029241af73cd5574a3cc204f
khashayarasghari <khasgharidev@gmail.com>
2024-05-17 02:05:07 +0330

    condition || - button tag - w-[] fixed

--- src/app/(header-footer)/scope/[scopeId]/news/Pagination.tsx
+++ src/app/(header-footer)/scope/[scopeId]/news/Pagination.tsx
@@ -8,27 +8,25 @@ interface paginationProps {
 }
 
 export default function Pagination(props: paginationProps) {
-    const pagesCount = Math.ceil(props.itemsCount / (props.perPage ? props.perPage : 8))
+    const pagesCount = Math.ceil(props.itemsCount / (props.perPage || 8))
 
     return (
         <div className="flex justify-center items-center gap-2 mx-auto">
             {/* right arrow */}
-            {(props.current > 1) &&
-                <div onClick={()=> props.handleClick((prev: number)=> prev -1)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
-                    <CaretRight />
-                </div>
-            }
+            <button onClick={() => props.handleClick((prev: number) => prev - 1)} className={`${props.current === 1 ? "pointer-events-none cursor-default opacity-40" : ""} flex justify-center items-center w-10 h-10 hover:bg-gray-200`}>
+                <CaretRight />
+            </button>
 
             {/* first page */}
             {(props.current >= 4) &&
-                <div onClick={()=> props.handleClick(1)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                <button onClick={() => props.handleClick(1)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                     1
-                </div>
+                </button>
             }
 
             {/* 3 dots between current and first page */}
             {(props.current >= 5) &&
-                <div className="flex justify-center items-center w-[40px] h-[40px]">
+                <div className="flex justify-center items-center w-10 h-10">
                     ...
                 </div>
             }
@@ -37,20 +35,20 @@ export default function Pagination(props: paginationProps) {
             {/* if it was on first page */}
             {(props.current === 1) &&
                 <div className="flex gap-2">
-                    <div className="flex justify-center items-center w-[40px] h-[40px] border hover:bg-gray-200">
+                    <button className="flex justify-center items-center w-10 h-10 border hover:bg-gray-200">
                         1
-                    </div>
+                    </button>
 
                     {pagesCount >= 2 &&
-                        <div onClick={()=> props.handleClick(2)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                        <button onClick={() => props.handleClick(2)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                             2
-                        </div>
+                        </button>
                     }
 
                     {pagesCount >= 3 &&
-                        <div onClick={()=> props.handleClick(3)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                        <button onClick={() => props.handleClick(3)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                             3
-                        </div>
+                        </button>
                     }
                 </div>
             }
@@ -58,24 +56,24 @@ export default function Pagination(props: paginationProps) {
             {/* if it was on second page */}
             {(props.current === 2) &&
                 <div className="flex gap-2">
-                    <div onClick={()=> props.handleClick(1)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                    <button onClick={() => props.handleClick(1)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                         1
-                    </div>
+                    </button>
 
-                    <div className="flex justify-center items-center w-[40px] h-[40px] border hover:bg-gray-200">
+                    <button className="flex justify-center items-center w-10 h-10 border hover:bg-gray-200">
                         2
-                    </div>
+                    </button>
 
                     {pagesCount >= 3 &&
-                        <div onClick={()=> props.handleClick(3)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                        <button onClick={() => props.handleClick(3)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                             3
-                        </div>
+                        </button>
                     }
 
                     {pagesCount >= 4 &&
-                        <div onClick={()=> props.handleClick(4)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                        <button onClick={() => props.handleClick(4)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                             4
-                        </div>
+                        </button>
                     }
                 </div>
             }
@@ -83,28 +81,28 @@ export default function Pagination(props: paginationProps) {
             {/* if it was on third page */}
             {(props.current === 3) &&
                 <div className="flex gap-2">
-                    <div onClick={()=> props.handleClick(1)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                    <button onClick={() => props.handleClick(1)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                         1
-                    </div>
+                    </button>
 
-                    <div onClick={()=> props.handleClick(2)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                    <button onClick={() => props.handleClick(2)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                         2
-                    </div>
+                    </button>
 
-                    <div className="flex justify-center items-center w-[40px] h-[40px] border hover:bg-gray-200">
+                    <button className="flex justify-center items-center w-10 h-10 border hover:bg-gray-200">
                         3
-                    </div>
+                    </button>
 
                     {pagesCount >= 4 &&
-                        <div  onClick={()=> props.handleClick(4)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                        <button onClick={() => props.handleClick(4)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                             4
-                        </div>
+                        </button>
                     }
 
                     {pagesCount >= 5 &&
-                        <div onClick={()=> props.handleClick(5)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                        <button onClick={() => props.handleClick(5)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                             5
-                        </div>
+                        </button>
                     }
                 </div>
             }
@@ -112,25 +110,25 @@ export default function Pagination(props: paginationProps) {
             {/* if it was on some middle pages */}
             {(props.current > 3 && props.current < pagesCount - 2) &&
                 <div className="flex gap-2">
-                    <div onClick={()=> props.handleClick(props.current - 2)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                    <button onClick={() => props.handleClick(props.current - 2)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                         {props.current - 2}
-                    </div>
+                    </button>
 
-                    <div onClick={()=> props.handleClick(props.current - 1)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                    <button onClick={() => props.handleClick(props.current - 1)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                         {props.current - 1}
-                    </div>
+                    </button>
 
-                    <div className="flex justify-center items-center w-[40px] h-[40px] border hover:bg-gray-200">
+                    <button className="flex justify-center items-center w-10 h-10 border hover:bg-gray-200">
                         {props.current}
-                    </div>
+                    </button>
 
-                    <div onClick={()=> props.handleClick(props.current + 1)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                    <button onClick={() => props.handleClick(props.current + 1)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                         {props.current + 1}
-                    </div>
+                    </button>
 
-                    <div onClick={()=> props.handleClick(props.current + 2)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                    <button onClick={() => props.handleClick(props.current + 2)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                         {props.current + 2}
-                    </div>
+                    </button>
                 </div>
             }
 
@@ -138,20 +136,20 @@ export default function Pagination(props: paginationProps) {
             {(props.current === pagesCount) &&
                 <div className="flex gap-2">
                     {(props.current - 2 > 0) &&
-                        <div onClick={()=> props.handleClick(props.current - 2)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                        <button onClick={() => props.handleClick(props.current - 2)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                             {props.current - 2}
-                        </div>
+                        </button>
                     }
 
                     {(props.current - 1 > 0) &&
-                        <div onClick={()=> props.handleClick(props.current - 1)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                        <button onClick={() => props.handleClick(props.current - 1)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                             {props.current - 1}
-                        </div>
+                        </button>
                     }
 
-                    <div className="flex justify-center items-center w-[40px] h-[40px] border hover:bg-gray-200">
+                    <button className="flex justify-center items-center w-10 h-10 border hover:bg-gray-200">
                         {props.current}
-                    </div>
+                    </button>
                 </div>
             }
 
@@ -159,24 +157,24 @@ export default function Pagination(props: paginationProps) {
             {(props.current === pagesCount - 1) &&
                 <div className="flex gap-2">
                     {(props.current - 2 > 0) &&
-                        <div onClick={()=> props.handleClick(props.current - 2)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                        <button onClick={() => props.handleClick(props.current - 2)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                             {props.current - 2}
-                        </div>
+                        </button>
                     }
 
-                    {(props.current - 1 > 0) && 
-                    <div onClick={()=> props.handleClick(props.current - 1)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
-                        {props.current - 1}
-                    </div>
+                    {(props.current - 1 > 0) &&
+                        <button onClick={() => props.handleClick(props.current - 1)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
+                            {props.current - 1}
+                        </button>
                     }
 
-                    <div className="flex justify-center items-center w-[40px] h-[40px] border hover:bg-gray-200">
+                    <button className="flex justify-center items-center w-10 h-10 border hover:bg-gray-200">
                         {props.current}
-                    </div>
+                    </button>
 
-                    <div onClick={()=> props.handleClick(pagesCount)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                    <button onClick={() => props.handleClick(pagesCount)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                         {pagesCount}
-                    </div>
+                    </button>
                 </div>
             }
 
@@ -184,51 +182,50 @@ export default function Pagination(props: paginationProps) {
             {(props.current === pagesCount - 2) &&
                 <div className="flex gap-2">
                     {(props.current - 2 > 0) &&
-                        <div onClick={()=> props.handleClick(props.current - 2)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                        <button onClick={() => props.handleClick(props.current - 2)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                             {props.current - 2}
-                        </div>
+                        </button>
                     }
 
-                    {(props.current - 1 > 0) && 
-                    <div onClick={()=> props.handleClick(props.current - 1)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
-                        {props.current - 1}
-                    </div>
+                    {(props.current - 1 > 0) &&
+                        <button onClick={() => props.handleClick(props.current - 1)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
+                            {props.current - 1}
+                        </button>
                     }
 
-                    <div className="flex justify-center items-center w-[40px] h-[40px] border hover:bg-gray-200">
+                    <button className="flex justify-center items-center w-10 h-10 border hover:bg-gray-200">
                         {props.current}
-                    </div>
+                    </button>
 
-                    <div onClick={()=> props.handleClick(props.current + 1)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                    <button onClick={() => props.handleClick(props.current + 1)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                         {props.current + 1}
-                    </div>
+                    </button>
 
-                    <div onClick={()=> props.handleClick(props.current + 2)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                    <button onClick={() => props.handleClick(props.current + 2)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                         {props.current + 2}
-                    </div>
+                    </button>
                 </div>
             }
 
             {/* 3 dots between current and last page */}
             {(props.current < pagesCount - 3) &&
-                <div className="flex justify-center items-center w-[40px] h-[40px]">
+                <div className="flex justify-center items-center w-10 h-10">
                     ...
                 </div>
             }
 
             {/* last page */}
             {(props.current < pagesCount - 2) &&
-                <div onClick={()=> props.handleClick(pagesCount)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
+                <button onClick={() => props.handleClick(pagesCount)} className="flex justify-center items-center w-10 h-10 hover:bg-gray-200">
                     {pagesCount}
-                </div>
+                </button>
             }
 
             {/* left arrow */}
-            {(props.current < pagesCount) &&
-                <div onClick={()=> props.handleClick((prev:number) => prev+1)} className="flex justify-center items-center w-[40px] h-[40px] hover:bg-gray-200">
-                    <CaretLeft />
-                </div>
-            }
+            <button onClick={() => props.handleClick((prev: number) => prev + 1)} className={`${props.current === pagesCount ? "pointer-events-none cursor-default opacity-40" : ""} flex justify-center items-center w-10 h-10 hover:bg-gray-200`}>
+                <CaretLeft />
+            </button>
+
         </div>
     )
 
