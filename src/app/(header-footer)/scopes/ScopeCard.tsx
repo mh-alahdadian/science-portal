999db6ecefc6a71e50d14b7cc71a37f3cfd57610
MohammadHossein Alahdadian <mhalah15@gmail.com>
2025-01-22 14:05:41 +0330

    handle already registerd scopes

--- src/app/(header-footer)/scopes/ScopeCard.tsx
+++ src/app/(header-footer)/scopes/ScopeCard.tsx
@@ -1,8 +1,8 @@
-import { getParsedToken, mutateService } from '@/api';
+import { getParsedToken, mutateService, queryService } from '@/api';
 import { Permission } from '@/constants';
 import { createFileUrl } from '@/utils';
 import { LockKey } from '@phosphor-icons/react';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { toast } from 'react-toastify';
@@ -23,8 +23,33 @@ function hasAccess(scopeId: number, permission?: Permission) {
 export default function ScopeCard({ scope }: Props) {
   const isLoggedIn = getParsedToken();
   const _hasAccess = hasAccess(scope.id!);
-  const { mutate: mutateMembershipRequest } = useMutation(mutateService('post', 'core:/v1/membership-requests'));
+  const queryClient = useQueryClient();
+  const requests = useQuery(queryService('core:/v1/membership-requests', {}));
+  const { mutate: mutateMembershipRequest } = useMutation({
+    ...mutateService('post', 'core:/v1/membership-requests'),
+    onSuccess() {
+      toast.success('درخواست شما ثبت شد.');
+      queryClient.invalidateQueries({ queryKey: queryService('core:/v1/membership-requests', {}).queryKey });
+    },
+    onError(e: any) {
+      toast.error(e.message);
+    },
+  });
+
   const Component = _hasAccess ? Link : 'div';
+
+  const membershipElement = requests.data?.find((x) => x.scopeId == scope.id!) ? (
+    <button className="btn-accent btn-outline min-w-40">در انتظار تایید</button>
+  ) : (
+    <button
+      className="btn-primary min-w-40"
+      onClick={() => {
+        mutateMembershipRequest({ body: { scopeId: scope.id } });
+      }}
+    >
+      عضویت
+    </button>
+  );
 
   return (
     <Component
@@ -37,26 +62,7 @@ export default function ScopeCard({ scope }: Props) {
       />
       <div className="text-2xl mb-auto">{scope.title}</div>
       {!_hasAccess && <LockKey weight="fill" className="absolute top-32" size={80} />}
-      {isLoggedIn && !_hasAccess && (
-        <button
-          className="btn-primary min-w-40"
-          onClick={() => {
-            mutateMembershipRequest(
-              { body: { scopeId: scope.id } },
-              {
-                onSuccess() {
-                  toast.success('درخواست شما ثبت شد.');
-                },
-                onError(e: any) {
-                  toast.error(e.message);
-                },
-              }
-            );
-          }}
-        >
-          عضویت
-        </button>
-      )}
+      {isLoggedIn && !_hasAccess && membershipElement}
     </Component>
   );
 }
