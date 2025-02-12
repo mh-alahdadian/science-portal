d3008b06bafc78491f4d5c257ca84439bdb77be3
Khashayar Asghari <48386500+KhashayarAsghari@users.noreply.github.com>
2025-02-12 10:56:27 +0330

    stack overflow page (#13)

---
+++ src/app/(header-footer)/scope/[scopeId]/block/components/LeftSideNav.tsx
@@ -0,0 +1,24 @@
+export default function LeftSideNav(){
+    return (
+      <div className="w-full">
+        <div className="w-9/12 mt-3 rounded-md mx-auto p-3 flex flex-col text-right !border-2-blockCogborder bg-blockCogBc">
+          <div className="flex flex-col gap-y-3">
+            <h2>بلاگ کاگنتیو</h2>
+            <div className="flex">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus aut amet omnis esse! Et id quo minima
+                dolorum nam earum vel numquam error? Veritatis obcaecati voluptatibus quae, expedita non quos?
+              </p>
+            </div>
+            <div className="flex">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic nemo corrupti deleniti, nam in assumenda
+                numquam suscipit beatae inventore earum cupiditate fugiat temporibus perspiciatis quam dolor eveniet
+                praesentium facere atque.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
